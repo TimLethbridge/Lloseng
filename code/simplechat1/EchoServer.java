f180a8c3d7bcaa18697b3c782c6319b4dcfe4cc8
Will Kennedy <54251976+TechnologicalStandpoint@users.noreply.github.com>
2020-05-20 20:50:28 -0400

    Added ocsf to the subdirectoy of simplechat1

--- code/simplechat1/EchoServer.java
+++ code/simplechat1/EchoServer.java
@@ -1,12 +1,12 @@
 // This file contains material supporting section 3.7 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
-// license found at www.lloseng.com 
+// license found at www.lloseng.com
 
 import java.io.*;
 import ocsf.server.*;
 
 /**
- * This class overrides some of the methods in the abstract 
+ * This class overrides some of the methods in the abstract
  * superclass in order to give more functionality to the server.
  *
  * @author Dr Timothy C. Lethbridge
@@ -15,30 +15,30 @@ import ocsf.server.*;
  * @author Paul Holden
  * @version July 2000
  */
-public class EchoServer extends AbstractServer 
+public class EchoServer extends AbstractServer
 {
   //Class variables *************************************************
-  
+
   /**
    * The default port to listen on.
    */
   final public static int DEFAULT_PORT = 5555;
-  
+
   //Constructors ****************************************************
-  
+
   /**
    * Constructs an instance of the echo server.
    *
    * @param port The port number to connect on.
    */
-  public EchoServer(int port) 
+  public EchoServer(int port)
   {
     super(port);
   }
 
-  
+
   //Instance methods ************************************************
-  
+
   /**
    * This method handles any messages received from the client.
    *
@@ -51,7 +51,7 @@ public class EchoServer extends AbstractServer
     System.out.println("Message received: " + msg + " from " + client);
     this.sendToAllClients(msg);
   }
-    
+
   /**
    * This method overrides the one in the superclass.  Called
    * when the server starts listening for connections.
@@ -61,7 +61,7 @@ public class EchoServer extends AbstractServer
     System.out.println
       ("Server listening for connections on port " + getPort());
   }
-  
+
   /**
    * This method overrides the one in the superclass.  Called
    * when the server stops listening for connections.
@@ -71,17 +71,17 @@ public class EchoServer extends AbstractServer
     System.out.println
       ("Server has stopped listening for connections.");
   }
-  
+
   //Class methods ***************************************************
-  
+
   /**
-   * This method is responsible for the creation of 
+   * This method is responsible for the creation of
    * the server instance (there is no UI in this phase).
    *
-   * @param args[0] The port number to listen on.  Defaults to 5555 
+   * @param args[0] The port number to listen on.  Defaults to 5555
    *          if no argument is entered.
    */
-  public static void main(String[] args) 
+  public static void main(String[] args)
   {
     int port = 0; //Port to listen on
 
@@ -93,14 +93,14 @@ public class EchoServer extends AbstractServer
     {
       port = DEFAULT_PORT; //Set port to 5555
     }
-	
+
     EchoServer sv = new EchoServer(port);
-    
-    try 
+
+    try
     {
       sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
+    }
+    catch (Exception ex)
     {
       System.out.println("ERROR - Could not listen for clients!");
     }

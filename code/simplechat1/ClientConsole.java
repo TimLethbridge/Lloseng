728438776191118f988827880478694919c5f7ae
Will Kennedy <54251976+TechnologicalStandpoint@users.noreply.github.com>
2020-05-24 13:35:36 -0400

    Changed client to terminate once the server shuts down

--- code/simplechat1/ClientConsole.java
+++ code/simplechat1/ClientConsole.java
@@ -1,6 +1,6 @@
 // This file contains material supporting section 3.7 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
-// license found at www.lloseng.com 
+// license found at www.lloseng.com
 
 import java.io.*;
 import client.*;
@@ -9,30 +9,30 @@ import common.*;
 /**
  * This class constructs the UI for a chat client.  It implements the
  * chat interface in order to activate the display() method.
- * Warning: Some of the code here is cloned in ServerConsole 
+ * Warning: Some of the code here is cloned in ServerConsole
  *
  * @author Fran&ccedil;ois B&eacute;langer
- * @author Dr Timothy C. Lethbridge  
+ * @author Dr Timothy C. Lethbridge
  * @author Dr Robert Lagani&egrave;re
  * @version July 2000
  */
-public class ClientConsole implements ChatIF 
+public class ClientConsole implements ChatIF
 {
   //Class variables *************************************************
-  
+
   /**
    * The default port to connect on.
    */
   final public static int DEFAULT_PORT = 5555;
-  
+
   //Instance variables **********************************************
-  
+
   /**
    * The instance of the client that created this ConsoleChat.
    */
   ChatClient client;
 
-  
+
   //Constructors ****************************************************
 
   /**
@@ -41,13 +41,13 @@ public class ClientConsole implements ChatIF
    * @param host The host to connect to.
    * @param port The port to connect on.
    */
-  public ClientConsole(String host, int port) 
+  public ClientConsole(String host, int port)
   {
-    try 
+    try
     {
       client= new ChatClient(host, port, this);
-    } 
-    catch(IOException exception) 
+    }
+    catch(IOException exception)
     {
       System.out.println("Error: Can't setup connection!"
                 + " Terminating client.");
@@ -55,28 +55,28 @@ public class ClientConsole implements ChatIF
     }
   }
 
-  
+
   //Instance methods ************************************************
-  
+
   /**
-   * This method waits for input from the console.  Once it is 
+   * This method waits for input from the console.  Once it is
    * received, it sends it to the client's message handler.
    */
-  public void accept() 
+  public void accept()
   {
     try
     {
-      BufferedReader fromConsole = 
+      BufferedReader fromConsole =
         new BufferedReader(new InputStreamReader(System.in));
       String message;
 
-      while (true) 
+      while (true)
       {
         message = fromConsole.readLine();
         client.handleMessageFromClientUI(message);
       }
-    } 
-    catch (Exception ex) 
+    }
+    catch (Exception ex)
     {
       System.out.println
         ("Unexpected error while reading from console!");
@@ -89,20 +89,20 @@ public class ClientConsole implements ChatIF
    *
    * @param message The string to be displayed.
    */
-  public void display(String message) 
+  public void display(String message)
   {
     System.out.println("> " + message);
   }
 
-  
+
   //Class methods ***************************************************
-  
+
   /**
    * This method is responsible for the creation of the Client UI.
    *
    * @param args[0] The host to connect to.
    */
-  public static void main(String[] args) 
+  public static void main(String[] args)
   {
     String host = "";
     int port = 0;  //The port number
@@ -110,12 +110,22 @@ public class ClientConsole implements ChatIF
     try
     {
       host = args[0];
+      port = Integer.parseInt(args[1]);
     }
     catch(ArrayIndexOutOfBoundsException e)
     {
       host = "localhost";
     }
-    ClientConsole chat= new ClientConsole(host, DEFAULT_PORT);
+
+    if (port==0)
+    {
+      ClientConsole chat= new ClientConsole(host, port);
+    }
+
+    else
+    {
+      ClientConsole chat= new ClientConsole(host, DEFAULT_PORT);
+    }
     chat.accept();  //Wait for console data
   }
 }

7530ba54bfbac8df5641b20f2c9e4be8ad421938
nikhil815 <npynd031@gmail.com>
2020-06-08 20:28:40 -0400

    Update ClientConsole.java

--- code/simplechat1/ClientConsole.java
+++ code/simplechat1/ClientConsole.java
@@ -118,7 +118,7 @@ public class ClientConsole implements ChatIF
 
     try
     {
-      this.display("Enter a port number");
+      
       port = Integer.parseInt(args[0]); //Get port from command line
       ClientConsole chat= new ClientConsole(host, port);
       chat.accept();

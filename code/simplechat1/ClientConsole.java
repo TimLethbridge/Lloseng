6bcd8513c52801e127824efb303f98b7c712e628
Leith Grira <lgrira12@gmail.com>
2020-06-14 19:13:06 -0400

    e5 a

--- code/simplechat1/ClientConsole.java
+++ code/simplechat1/ClientConsole.java
@@ -125,7 +125,7 @@ public class ClientConsole implements ChatIF
 
 
   protected void connectionClosed(){
-    if (!isConnected) {
+    if (isConnected == false) {
       System.out.println("The serever has been disconnected...");
       System.exit(1);
     }

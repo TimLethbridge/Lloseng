b12626f7444f703b6ea1977ff269ef9ce10db451
Leith Grira <lgrira12@gmail.com>
2020-06-15 15:03:52 -0400

    test7

--- code/simplechat1/ClientConsole.java
+++ code/simplechat1/ClientConsole.java
@@ -185,13 +185,7 @@ public class ClientConsole implements ChatIF
 
     ClientConsole chat= new ClientConsole(host, port);
     chat.accept();  //Wait for console data
-
-    final public void setHost(String host1) {
-    host = host1;
-  }
-    final public void setPort(int port1) {
-    port = port1;
-  }
+    
   }
 
 

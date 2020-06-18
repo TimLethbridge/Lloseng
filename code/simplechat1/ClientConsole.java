63bd2a5c01810c4257785195368a0d3c4935e87a
ParvaP <parvapathak2001@gmail.com>
2020-06-17 21:53:47 -0400

    Finished the Assignment

--- code/simplechat1/ClientConsole.java
+++ code/simplechat1/ClientConsole.java
@@ -7,33 +7,32 @@ import client.*;
 import common.*;
 
 /**
- * This class constructs the UI for a chat client.  It implements the
- * chat interface in order to activate the display() method.
- * Warning: Some of the code here is cloned in ServerConsole 
+ * This class constructs the UI for a chat client. It implements the chat
+ * interface in order to activate the display() method. Warning: Some of the
+ * code here is cloned in ServerConsole
  *
  * @author Fran&ccedil;ois B&eacute;langer
- * @author Dr Timothy C. Lethbridge  
+ * @author Dr Timothy C. Lethbridge
  * @author Dr Robert Lagani&egrave;re
  * @version July 2000
  */
-public class ClientConsole implements ChatIF 
-{
-  //Class variables *************************************************
-  
+public class ClientConsole implements ChatIF {
+  // Class variables *************************************************
+
   /**
    * The default port to connect on.
    */
   final public static int DEFAULT_PORT = 5555;
-  
-  //Instance variables **********************************************
-  
+
+  // Instance variables **********************************************
+
   /**
    * The instance of the client that created this ConsoleChat.
    */
   ChatClient client;
+  boolean loggedIn;
 
-  
-  //Constructors ****************************************************
+  // Constructors ****************************************************
 
   /**
    * Constructs an instance of the ClientConsole UI.
@@ -41,82 +40,109 @@ public class ClientConsole implements ChatIF
    * @param host The host to connect to.
    * @param port The port to connect on.
    */
-  public ClientConsole(String host, int port) 
-  {
-    try 
-    {
-      client= new ChatClient(host, port, this);
-    } 
-    catch(IOException exception) 
-    {
-      System.out.println("Error: Can't setup connection!"
-                + " Terminating client.");
+  public ClientConsole(String host, int port, String loginId) {
+    try {
+      client = new ChatClient(host, port, loginId, this);
+      this.loggedIn = true;
+    } catch (IOException exception) {
+      System.out.println("Error: Can't setup connection!" + " Terminating client.");
       System.exit(1);
     }
   }
 
-  
-  //Instance methods ************************************************
-  
-  /**
-   * This method waits for input from the console.  Once it is 
-   * received, it sends it to the client's message handler.
-   */
-  public void accept() 
-  {
-    try
-    {
-      BufferedReader fromConsole = 
-        new BufferedReader(new InputStreamReader(System.in));
-      String message;
+  // Instance methods ************************************************
 
-      while (true) 
-      {
-        message = fromConsole.readLine();
-        client.handleMessageFromClientUI(message);
+  /**
+   * This method waits for input from the console. Once it is received, it sends
+   * it to the client's message handler.
+   */
+  public void accept() {
+    while (true) {
+      try {
+        BufferedReader fromConsole = new BufferedReader(new InputStreamReader(System.in));
+        String message;
+        String[] messageS;
+        while (true) {
+          message = fromConsole.readLine();
+          messageS = message.split(" ");
+          if (message.charAt(0) == '#') {
+            if (message.equals("#quit")) {
+              client.quit();
+              System.exit(0);
+            } else if (message.equals("#logoff")) {
+              if (!loggedIn){
+              display("Error, already logged off.");
+              }
+              loggedIn = false;
+            } else if (messageS[0].equals("#sethost")) {
+              if (!loggedIn) {
+                client.setHost(messageS[1]);
+              }
+            } else if (messageS[0].equals("#setport")) {
+              if (!loggedIn) {
+                client.setPort(Integer.parseInt(messageS[1]));
+              }
+            } else if (messageS[0].equals("#login")) {
+              if (loggedIn){
+                display("Error, already logged in.");
+              }
+              loggedIn = true;
+            } else if (message.equals("#gethost")) {
+              display(client.getHost());
+            } else if (message.equals("#getport")) {
+              display(Integer.toString(client.getPort()));
+            } else {
+              display("Invalid Command");
+            }
+          } else {
+            if (loggedIn) {
+              client.handleMessageFromClientUI(message);
+            }
+          }
+        }
+      } catch (Exception ex) {
+        System.out.println("Unexpected error while reading from console!");
       }
-    } 
-    catch (Exception ex) 
-    {
-      System.out.println
-        ("Unexpected error while reading from console!");
     }
   }
 
   /**
-   * This method overrides the method in the ChatIF interface.  It
-   * displays a message onto the screen.
+   * This method overrides the method in the ChatIF interface. It displays a
+   * message onto the screen.
    *
    * @param message The string to be displayed.
    */
-  public void display(String message) 
-  {
+  public void display(String message) {
     System.out.println("> " + message);
   }
 
-  
-  //Class methods ***************************************************
-  
+  // Class methods ***************************************************
+
   /**
    * This method is responsible for the creation of the Client UI.
    *
    * @param args[0] The host to connect to.
    */
-  public static void main(String[] args) 
-  {
+  public static void main(String[] args) {
     String host = "";
-    int port = 0;  //The port number
+    int port = 0; // The port number
 
-    try
-    {
-      host = args[0];
+    try {
+      host = args[2];
+    } catch (ArrayIndexOutOfBoundsException e) {
+      host.equals("localhost");
     }
-    catch(ArrayIndexOutOfBoundsException e)
-    {
-      host = "localhost";
+    try {
+      port = Integer.parseInt(args[1]);
+    } catch (ArrayIndexOutOfBoundsException e) {
+      port = DEFAULT_PORT;
     }
-    ClientConsole chat= new ClientConsole(host, DEFAULT_PORT);
-    chat.accept();  //Wait for console data
+    if (args.length != 0) {
+      ClientConsole chat = new ClientConsole(host, port, args[0]);
+      chat.accept(); // Wait for console data
+    } else {
+      System.out.println("Did not enter login ID.");
+    }
   }
 }
-//End of ConsoleChat class
+// End of ConsoleChat class
--- code/simplechat1/EchoServer.java
+++ code/simplechat1/EchoServer.java
@@ -3,11 +3,12 @@
 // license found at www.lloseng.com 
 
 import java.io.*;
+import common.ChatIF;
 import ocsf.server.*;
 
 /**
- * This class overrides some of the methods in the abstract 
- * superclass in order to give more functionality to the server.
+ * This class overrides some of the methods in the abstract superclass in order
+ * to give more functionality to the server.
  *
  * @author Dr Timothy C. Lethbridge
  * @author Dr Robert Lagani&egrave;re
@@ -15,95 +16,106 @@ import ocsf.server.*;
  * @author Paul Holden
  * @version July 2000
  */
-public class EchoServer extends AbstractServer 
-{
-  //Class variables *************************************************
-  
+public class EchoServer extends AbstractServer {
+  // Class variables *************************************************
+
   /**
    * The default port to listen on.
    */
   final public static int DEFAULT_PORT = 5555;
-  
-  //Constructors ****************************************************
-  
+
+  // Constructors ****************************************************
+
   /**
    * Constructs an instance of the echo server.
    *
    * @param port The port number to connect on.
    */
-  public EchoServer(int port) 
-  {
+  public EchoServer(int port, ChatIF serverUI) {
     super(port);
+    this.serverUI = serverUI;
   }
 
-  
-  //Instance methods ************************************************
-  
+  ChatIF serverUI;
+
+  // Instance methods ************************************************
+
   /**
    * This method handles any messages received from the client.
    *
-   * @param msg The message received from the client.
+   * @param msg    The message received from the client.
    * @param client The connection from which the message originated.
    */
-  public void handleMessageFromClient
-    (Object msg, ConnectionToClient client)
-  {
+  public void handleMessageFromClient(Object msg, ConnectionToClient client) {
+    String[] msgS = msg.toString().split(" ");
     System.out.println("Message received: " + msg + " from " + client);
-    this.sendToAllClients(msg);
-  }
-    
-  /**
-   * This method overrides the one in the superclass.  Called
-   * when the server starts listening for connections.
-   */
-  protected void serverStarted()
-  {
-    System.out.println
-      ("Server listening for connections on port " + getPort());
-  }
-  
-  /**
-   * This method overrides the one in the superclass.  Called
-   * when the server stops listening for connections.
-   */
-  protected void serverStopped()
-  {
-    System.out.println
-      ("Server has stopped listening for connections.");
-  }
-  
-  //Class methods ***************************************************
-  
-  /**
-   * This method is responsible for the creation of 
-   * the server instance (there is no UI in this phase).
-   *
-   * @param args[0] The port number to listen on.  Defaults to 5555 
-   *          if no argument is entered.
-   */
-  public static void main(String[] args) 
-  {
-    int port = 0; //Port to listen on
 
-    try
-    {
-      port = Integer.parseInt(args[0]); //Get port from command line
-    }
-    catch(Throwable t)
-    {
-      port = DEFAULT_PORT; //Set port to 5555
-    }
-	
-    EchoServer sv = new EchoServer(port);
-    
-    try 
-    {
-      sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
-    {
-      System.out.println("ERROR - Could not listen for clients!");
+    if (client.getInfo("first") == null) {
+      if (msgS[0].equals("#login")) {
+        if (!msgS[1].equals(null)) {
+          client.setInfo("loginId", msgS[1]);
+          client.setInfo("first", "T");
+        }
+      } else {
+        try {
+          client.sendToClient("Error");
+        } catch (IOException e) {
+          System.out.println("Error");
+        }
+        try {
+          client.close();
+        } catch (IOException e) {
+          System.out.println("Error");
+        }
+      }
+    } else {
+      if (msgS[0].equals("#login")) {
+        try {
+          client.sendToClient("Error");
+        } catch (IOException e) {
+          System.out.println("Error");
+        }
+      }
+      this.sendToAllClients(client.getInfo("loginId") +": "+msg);
     }
   }
+
+  /**
+   * This method overrides the one in the superclass. Called when the server
+   * starts listening for connections.
+   */
+  protected void serverStarted() {
+    System.out.println("Server listening for connections on port " + getPort());
+  }
+
+  /**
+   * This method overrides the one in the superclass. Called when the server stops
+   * listening for connections.
+   */
+  protected void serverStopped() {
+    System.out.println("Server has stopped listening for connections.");
+  }
+
+  synchronized protected void clientException(ConnectionToClient client, Throwable exception) {
+    System.out.println("A client has disconnected.");
+  }
+
+  synchronized protected void clientDisconnected(ConnectionToClient client) {
+    System.out.println("A client has disconnected.");
+  }
+
+  protected void clientConnected(ConnectionToClient client) {
+    System.out.println("A client has connected.");
+  }
+
+  // Class methods ***************************************************
+
+  /**
+   * This method is responsible for the creation of the server instance (there is
+   * no UI in this phase).
+   *
+   * @param args[0] The port number to listen on. Defaults to 5555 if no argument
+   *                is entered.
+   */
 }
-//End of EchoServer class
+// End of EchoServer class

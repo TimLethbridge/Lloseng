c7dc501c41457f68f7b0c53d867bf8fdb1689fd9
Shivam Syal <ssyal030@uottawa.ca>
2020-06-18 21:05:48 -0400

    This update has the changes for e6 and e7 as well as a file called ShivamSyal_300065225_ssyal030@uottawa.ca.pdf for the e8 test cases

--- code/simplechat1/EchoServer.java
+++ code/simplechat1/EchoServer.java
@@ -3,6 +3,7 @@
 // license found at www.lloseng.com 
 
 import java.io.*;
+import common.ChatIF;
 import ocsf.server.*;
 
 /**
@@ -31,11 +32,15 @@ public class EchoServer extends AbstractServer
    *
    * @param port The port number to connect on.
    */
-  public EchoServer(int port) 
+  public EchoServer(int port, ChatIF serverUI) 
   {
     super(port);
+    this.serverUI = serverUI;
+    
   }
 
+  ChatIF serverUI;
+  
   
   //Instance methods ************************************************
   
@@ -45,33 +50,48 @@ public class EchoServer extends AbstractServer
    * @param msg The message received from the client.
    * @param client The connection from which the message originated.
    */
-  public void handleMessageFromClient
-    (Object msg, ConnectionToClient client)
-  {
-    System.out.println("Message received: " + msg + " from " + client);
-    this.sendToAllClients(msg);
+  public void handleMessageFromClient(Object msg, ConnectionToClient client){
+      String temp = msg.toString();
+      if(temp.contains("#login")){
+        System.out.println(msg);
+        this.sendToAllClients("ServerMSG> Welcome to the server " + temp.split(" ")[1]+".");
+        client.setInfo("Login ID",temp.split(" ")[1]);
+      }else{
+        System.out.println("Message received: " + msg + " from " + client);
+        this.sendToAllClients(client.getInfo("Login ID")+": "+msg);
+      }
   }
+  
     
-  /**
-   * This method overrides the one in the superclass.  Called
-   * when the server starts listening for connections.
-   */
-  protected void serverStarted()
-  {
-    System.out.println
-      ("Server listening for connections on port " + getPort());
+  protected void serverStarted(){
+    System.out.println("Server listening for connections on port # " + getPort());
+    try{
+      listen();
+    }catch(IOException exception){
+      System.out.println("ERROR.");
+    }
+  }
+  protected void serverStopped(){
+    stopListening();
+    System.out.println("Server has stopped listening for connections.");
+    this.sendToAllClients("Server stopped listening for connections.");
+    
+  }
+
+  protected void clientConnected(ConnectionToClient client) {
+    System.out.println("A new client has connected to the server.");
+  }
+
+  synchronized protected void clientException(ConnectionToClient client, Throwable exception) {
+    System.out.println(client.getInfo("Login ID")+" has left the server.");
+  }
+
+  synchronized protected void clientDisconnected(ConnectionToClient client) {
+    System.out.println(client.getInfo("Login ID")+" has left the server.");
   }
   
-  /**
-   * This method overrides the one in the superclass.  Called
-   * when the server stops listening for connections.
-   */
-  protected void serverStopped()
-  {
-    System.out.println
-      ("Server has stopped listening for connections.");
-  }
   
+
   //Class methods ***************************************************
   
   /**
@@ -81,41 +101,5 @@ public class EchoServer extends AbstractServer
    * @param args[0] The port number to listen on.  Defaults to 5555 
    *          if no argument is entered.
    */
-  public static void main(String[] args) 
-  {
-    int port = 0; //Port to listen on
-
-    try
-    {
-      port = Integer.parseInt(args[0]); //Get port from command line
-    }
-    catch(Throwable t)
-    {
-      port = DEFAULT_PORT; //Set port to 5555
-    }
-	
-    EchoServer sv = new EchoServer(port);
-    
-    try 
-    {
-      sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
-    {
-      System.out.println("ERROR - Could not listen for clients!");
-    }
-  }
-  protected void clientConnected(ConnectionToClient client) {
-    System.out.println("New client entered server.");
-    this.sendToAllClients("Welcome to the server new client!");
-  }
-  synchronized protected void clientDisconnected(ConnectionToClient client) {
-    System.out.println("Client left the server.");
-    this.sendToAllClients("Sadly, a client has client the server.");
-  }
-  synchronized protected void clientException (ConnectionToClient client, Throwable exception) {
-    System.out.println("Client left the server.");
-    this.sendToAllClients("Sadly, a client has client the server.");
-  }
 }
 //End of EchoServer class

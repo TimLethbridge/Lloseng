0162ce1de6f24919570d7d86349dbeb86927e172
Abilaash <39958409+Abilaash01@users.noreply.github.com>
2020-06-20 21:08:27 -0400

    Main method was removed from this file and the clientConnected and clientDisconnected methods were implemented. The handleMessageFromClient method was modified to detect when users login for the first time or anytime after that and stores their information for later use. loginID was also created to be used to reference the loginID of individual clients when receiving their data.

--- code/simplechat1/EchoServer.java
+++ code/simplechat1/EchoServer.java
@@ -4,6 +4,7 @@
 
 import java.io.*;
 import ocsf.server.*;
+import common.*;
 
 /**
  * This class overrides some of the methods in the abstract 
@@ -23,6 +24,8 @@ public class EchoServer extends AbstractServer
    * The default port to listen on.
    */
   final public static int DEFAULT_PORT = 5555;
+  private static String loginID;
+  ChatIF serverUI;
   
   //Constructors ****************************************************
   
@@ -31,9 +34,10 @@ public class EchoServer extends AbstractServer
    *
    * @param port The port number to connect on.
    */
-  public EchoServer(int port) 
+  public EchoServer(int port, ChatIF serverUI) 
   {
     super(port);
+	this.serverUI = serverUI;
   }
 
   
@@ -45,12 +49,20 @@ public class EchoServer extends AbstractServer
    * @param msg The message received from the client.
    * @param client The connection from which the message originated.
    */
-  public void handleMessageFromClient
-    (Object msg, ConnectionToClient client)
-  {
-    System.out.println("Message received: " + msg + " from " + client);
-    this.sendToAllClients(msg);
-  }
+	public void handleMessageFromClient
+		(Object msg, ConnectionToClient client)
+	{
+		String temp[] = String.valueOf(msg).split(" ");
+		
+		if(temp[0].equals("#login") && temp.length > 1) {
+			client.setInfo(loginID, temp[1]);
+			this.sendToAllClients(client.getInfo(loginID) + " has logged on");
+			System.out.println("Message received: " + msg + " from " + client);
+		} else {
+			System.out.println("Message received: " + msg + " from " + client);
+			this.sendToAllClients(client.getInfo(loginID) + "> " + msg);
+		}
+	}
     
   /**
    * This method overrides the one in the superclass.  Called
@@ -81,29 +93,13 @@ public class EchoServer extends AbstractServer
    * @param args[0] The port number to listen on.  Defaults to 5555 
    *          if no argument is entered.
    */
-  public static void main(String[] args) 
-  {
-    int port = 0; //Port to listen on
-
-    try
-    {
-      port = Integer.parseInt(args[0]); //Get port from command line
-    }
-    catch(Throwable t)
-    {
-      port = DEFAULT_PORT; //Set port to 5555
-    }
-	
-    EchoServer sv = new EchoServer(port);
-    
-    try 
-    {
-      sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
-    {
-      System.out.println("ERROR - Could not listen for clients!");
-    }
+  synchronized protected void clientConnected(ConnectionToClient client) {
+	System.out.println("A client has connected");
+  }
+  
+  synchronized protected void clientDisconnected(ConnectionToClient client) {
+	System.out.println("A client has disconnected");
+	this.sendToAllClients(client.getInfo(loginID) + " has logged off");
   }
 }
 //End of EchoServer class

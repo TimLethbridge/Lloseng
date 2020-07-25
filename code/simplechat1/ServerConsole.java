323cddb63a6c491423a33a8d68b2b4f1b3b4825c
qaiys <56705365+qaiys@users.noreply.github.com>
2020-07-24 20:11:42 -0400

    Create ServerConsole.java

---
+++ code/simplechat1/ServerConsole.java
@@ -0,0 +1,129 @@
+// This file contains material supporting section 3.7 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at www.lloseng.com 
+
+import java.io.*;
+import client.*;
+import common.*;
+
+/**
+ * This class constructs the UI for a chat client.  It implements the
+ * chat interface in order to activate the display() method.
+ * Warning: Some of the code here is cloned in ServerConsole 
+ *
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Dr Timothy C. Lethbridge  
+ * @author Dr Robert Lagani&egrave;re
+ * @version July 2000
+ */
+public class ServerConsole implements ChatIF 
+{
+  //Class variables *************************************************
+  
+  /**
+   * The default port to connect on.
+   */
+  final public static int DEFAULT_PORT = 5555;
+  
+  //Instance variables **********************************************
+  
+  /**
+   * The instance of the client that created this ConsoEchoServerleChat.
+   */
+  EchoServer server;
+
+  
+  //Constructors ****************************************************
+
+  /**
+   * Constructs an instance of the ClientConsole UI.
+   *
+   * @param host The host to connect to.
+   * @param port The port to connect on.
+   */
+  public ServerConsole(int port) 
+  {
+    try 
+    {
+      server= new EchoServer(port);
+      server.listen();
+    } 
+    catch(Exception exception) 
+    {
+      System.out.println("Error: Can't setup connection!"
+                + " Terminating client.");
+      System.exit(1);
+    }
+  }
+
+  
+  //Instance methods ************************************************
+  
+  /**
+   * This method waits for input from the console.  Once it is 
+   * received, it sends it to the client's message handler.
+   */
+  public void accept() 
+  {
+    try
+    {
+      BufferedReader fromConsole = 
+        new BufferedReader(new InputStreamReader(System.in));
+      String message;
+
+      while (true) 
+      {
+        message = fromConsole.readLine();
+        server.handleMessageFromServerUI(message);
+      }
+    } 
+    catch (Exception ex) 
+    {
+      System.out.println
+        ("Unexpected error while reading from console!");
+    }
+  }
+
+  /**
+   * This method overrides the method in the ChatIF interface.  It
+   * displays a message onto the screen.
+   *
+   * @param message The string to be displayed.
+   */
+  public void display(String message) 
+  {
+    System.out.println("> " + message);
+  }
+
+  
+  //Class methods ***************************************************
+  
+  /**
+   * This method is responsible for the creation of the Client UI.
+   *
+   * @param args[0] The host to connect to.
+   */
+  public static void main(String[] args) 
+  {
+    int port = 0;  //The port number
+    try
+    {
+      port = Integer.parseInt(args[0]);
+    }
+    catch(ArrayIndexOutOfBoundsException e)
+    {
+      port = 5555;
+    }
+
+    ServerConsole sv = new ServerConsole(port);
+   try{
+       sv.accept();
+   }catch (Exception ex){
+   }
+  }
+
+  public void quit(){
+    System.exit(0);
+  }
+}
+//End of ConsoleChat class
